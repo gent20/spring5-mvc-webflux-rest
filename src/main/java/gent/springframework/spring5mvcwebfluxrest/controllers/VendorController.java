f551e63c04274697d9f03952148854e1abf270aa
gent20 <123qweasdbnM>
2020-08-19 23:04:47 +0200

    Added PUT Method and Create Method in Vendor Controller, VendorControllerTest
    ## LAST COMMIT WAS PUT AND CREATE FOR Category not Vendors ##

--- src/main/java/gent/springframework/spring5mvcwebfluxrest/controllers/VendorController.java
+++ src/main/java/gent/springframework/spring5mvcwebfluxrest/controllers/VendorController.java
@@ -3,9 +3,9 @@ package gent.springframework.spring5mvcwebfluxrest.controllers;
 
 import gent.springframework.spring5mvcwebfluxrest.domain.Vendor;
 import gent.springframework.spring5mvcwebfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,5 +26,18 @@ public class VendorController {
     @GetMapping("/api/v1/vendors/{id}")
     Mono<Vendor> getVendorById(@PathVariable String id){
         return  vendorRepository.findById(id);
+    }
+
+    @PostMapping("/api/v1/vendors")
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Void> createVendor(@RequestBody Publisher<Vendor> vendorStream){
+        return vendorRepository.saveAll(vendorStream).then();
+    }
+
+    @PutMapping("/api/v1/vendors/{id}")
+    Mono<Vendor> createOrUpdateVendor(@PathVariable String id , @RequestBody  Vendor vendor){
+        vendor.setId(id);
+        return  vendorRepository.save(vendor);
+
     }
 }
--- src/test/java/gent/springframework/spring5mvcwebfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/gent/springframework/spring5mvcwebfluxrest/controllers/VendorControllerTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 class VendorControllerTest {
 
@@ -45,5 +48,35 @@ class VendorControllerTest {
                 .uri("/api/v1/vendors/someid")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    void testCreate(){
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Mono<Vendor> vendorToCreate = Mono.just(Vendor.builder().firstname("Gent").lastname("Kastrati").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToCreate , Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    void testUpdate(){
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().build()));
+
+        Mono<Vendor> vendorToUpdate = Mono.just(Vendor.builder().build());
+
+        webTestClient.put()
+                .uri("/api/v1/vendors/ghgh")
+                .body(vendorToUpdate , Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
