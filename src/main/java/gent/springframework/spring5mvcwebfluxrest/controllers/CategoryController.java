897fa34612283506f60b13cd86e0ba628fb7540e
gent20 <123qweasdbnM>
2020-08-19 22:35:46 +0200

    Added PUT Method and Create Method in Vendor Controller VendorControllerTest

--- src/main/java/gent/springframework/spring5mvcwebfluxrest/controllers/CategoryController.java
+++ src/main/java/gent/springframework/spring5mvcwebfluxrest/controllers/CategoryController.java
@@ -2,9 +2,9 @@ package gent.springframework.spring5mvcwebfluxrest.controllers;
 
 import gent.springframework.spring5mvcwebfluxrest.domain.Category;
 import gent.springframework.spring5mvcwebfluxrest.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,5 +26,17 @@ public class CategoryController {
         return categoryRepository.findById(id);
     }
 
+
+    @PostMapping("/api/v1/categories")
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Void> createCategory(@RequestBody Publisher<Category> categoryStream){
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+
+    @PutMapping("/api/v1/categories/{id}")
+    Mono<Category> updateCategory(@PathVariable String id , @RequestBody Category category){
+        category.setId(id);
+        return categoryRepository.save(category);
+    }
 
 }
